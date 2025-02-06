9b479983f73e9dad2ca7b79c96fe6268c154b93f
AdarshThakur398 <nutius90@gmail.com>
2025-02-06 18:09:55 +0530

    added more containers and appended widget inside mainContainer

--- widget.js
+++ widget.js
@@ -22,6 +22,20 @@
         this.video.muted=true;
         this.video.autoplay=true;
         this.video.loop=true;
-        
+
+        this.cta=document.createElement('button');
+        this.cta.className='cta';
+        this.cta.textContent=this.config.ctaText;
+        this.container.appendChild(this.video);
+        this.container.appendChild(this.cta);
+        this.addEventListeners();
+
+        const targetContainer=document.getElementById(this.config.containerId);
+        if(targetContainer) {
+            targetContainer.appendChild(this.container);
+        }
+        this.valdateDuration();
+
     }
-})
+    window.VideoWidget = VideoWidget;
+})();
